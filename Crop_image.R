440f989f0bf48c6be2b1fb98ae3da5fe5e3a09d1
Zulkarnain <M.Zulkarnain@cgiar.org>
2019-03-27 00:32:14 +0700

    modify cropping image and adding text in cropped image

--- Crop_image.R
+++ Crop_image.R
@@ -4,8 +4,10 @@ library(devtools)
 source_gist("https://gist.github.com/mrdwab/6424112")
 
 #readtable
-selectedImage_table <- read.csv("E:/R+/Crowdsourcing/crop_image/selected_images.csv", header = TRUE, sep =",")
+selectedImage_table <- read.csv("E:/R+/Crowdsourcing/crop_image/points/XYmatched_selected_images_updated_loc2.csv", header = TRUE, sep =",")
 selectedImage_table$pilename <- "ordinarypile"
+
+#create stratified random
 random <- stratified(selectedImage_table,"pilename",0.2)
 random$pilename <- "expertpile"
 random_subset <- subset(random, select=c(id_new, imageid, pilename))
@@ -17,37 +19,39 @@ write.table(selectedImage_table1, file = "E:/R+/Crowdsourcing/crop_image/selecte
 dir.create("E:/R+/Crowdsourcing/crop_image/expertpile", mode = '0777')
 dir.create("E:/R+/Crowdsourcing/crop_image/ordinarypile", mode = '0777')
 
-for (n in 1:length(unique(selectedImage_table1$lc2017_lev2id))){
-  filtertable1 <- subset(selectedImage_table,selectedImage_table$lc2017_lev2id == n)
-  dir.create(paste0("E:/R+/Crowdsourcing/crop_image/expertpile/",unique(filtertable1$lc2017_lev2_class)))
+for (n in 1:length(unique(selectedImage_table$lc17id))){
+  filtertable1 <- subset(selectedImage_table,selectedImage_table$lc17id == n)
+  dir.create(paste0("E:/R+/Crowdsourcing/crop_image/expertpile/",unique(filtertable1$lc17class)))
 }
 
-for (n in 1:length(unique(selectedImage_table$lc2017_lev2id))){
-  filtertable1 <- subset(selectedImage_table,selectedImage_table$lc2017_lev2id == n)
-  dir.create(paste0("E:/R+/Crowdsourcing/crop_image/ordinarypile/",unique(filtertable1$lc2017_lev2_class)))
+for (n in 1:length(unique(selectedImage_table$lc17id))){
+  filtertable1 <- subset(selectedImage_table,selectedImage_table$lc17id == n)
+  dir.create(paste0("E:/R+/Crowdsourcing/crop_image/ordinarypile/",unique(filtertable1$lc17class)))
 }
 
-for (n in 1:length(unique(selectedImage_table1$LC_Lev2_id))){
-  filtertable1 <- subset(selectedImage_table,selectedImage_table$LC_Lev2_id == n)
-  dir.create(paste0("E:/R+/Crowdsourcing/crop_image/gpspile/",unique(filtertable1$LC_Lev2)))
+for (n in 1:length(unique(selectedImage_table$lc17id))){
+  filtertable1 <- subset(selectedImage_table,selectedImage_table$lc17id == n)
+  dir.create(paste0("E:/R+/Crowdsourcing/crop_image/gpspile/",unique(filtertable1$lc17class)))
 }
 
-#Crop image 512x512 pixel (Batch)
-for (i in 1:nrow(matched_selected_image1)){
-  filtertable2 <- subset(matched_selected_image1,matched_selected_image1$id_new == i)
+#Crop image 400x400 pixel (Batch)
+for (i in 1:nrow(selectedImage_table)){
+  filtertable2 <- subset(selectedImage_table,selectedImage_table$id_new == n)
   image_name <- paste0("E:/R+/Crowdsourcing/crop_image/", filtertable2$filename)
   if(file.exists(image_name)){
     image1 <- image_read(image_name)
     crop1 <- image_crop(image1, geometry_area(width = 400, height = 400, x_off = 824, y_off = 824))
-    locimg <- paste0("lokasi gambar: kecamatan test")
-    imgtext1 <- image_annotate(crop1, locimg, gravity = "South", size = 18, font = "Comic Sans", color = "white")
-    export <- writeImage(crop1, paste0("E:/R+/Crowdsourcing/crop_image/", unique(filtertable2$pilename), "/", unique(filtertable2$lc2017_lev2_class),"/cropped_",filtertable2$filename))
+    locimg <- paste("Lokasi: KEC. ", filtertable2$kecamatan, ", 
+                    KAB. ", filtertable2$kabupaten)
+    imgtext1 <- image_annotate(crop1, locimg, gravity = "South", size = 13, font = "Palatino", color = "white")
+    export <- image_write(imgtext1, paste0("E:/R+/Crowdsourcing/crop_image/", unique(filtertable2$pilename), "/", unique(filtertable2$lc17class),"/cropped_",filtertable2$filename))
   }
 }
 
+#Testing
 #image1 <- image_read("E:/R+/Crowdsourcing/test_magick/ff728ffb-f62c-f568-83d8-5ad74fa6a583_110_0_20180601_034337.jpg")
-#crop1 <- image_crop(img1, geometry_area(width = 400, height = 400, x_off = 824, y_off = 824))
-#locimg <- paste0("Lokasi gambar: Kecamatan Test")
-#imgtext <- image_annotate(crop1, locimg, gravity = "South", size = 18, font = "Comic Sans", color = "white")
+#crop1 <- image_crop(image1, geometry_area(width = 400, height = 400, x_off = 824, y_off = 824))
+#locimg <- paste0("Lokasi: KEC WARKUKRANAU SELATAN, KAB. OGANKOMERINGULU SELATAN")
+#imgtext <- image_annotate(crop1, locimg, gravity = "South", size = 14, font = "Palatino", color = "white")
 #export <- image_write(imgtext, "E:/R+/Crowdsourcing/test_magick/testext6_400x400.jpg")
 
